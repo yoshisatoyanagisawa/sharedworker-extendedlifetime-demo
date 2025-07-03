1d9881eae2bfed40c27cab71dd682f0c6b13a652
Yoshisato Yanagisawa <yoshisato.yanagisawa@gmail.com>
2025-07-04 08:52:29 +0900

    Update multiply.js
    Added the new SharedWorker support

--- multiply.js
+++ multiply.js
@@ -1,22 +1,31 @@
-const first = document.querySelector("#number1");
-const second = document.querySelector("#number2");
+const num1 = document.querySelector("#number1");
+const num2 = document.querySelector("#number2");
+const num3 = document.querySelector("#number3");
+const num4 = document.querySelector("#number4");
 
 const result1 = document.querySelector(".result1");
+const result1 = document.querySelector(".result2");
 
-if (!!window.SharedWorker) {
-  // Create with the extendedLifetime option.
-  const myWorker = new SharedWorker("worker.js", {extendedLifetime: true});
-
-  [first, second].forEach((input) => {
+function setup(label, worker, field1, field2, result) {
+  [field1, field2].forEach((input) => {
     input.onchange = () => {
-      myWorker.port.postMessage([first.value, second.value]);
-      console.log("Message posted to worker");
+      myWorker.port.postMessage([field1.value, field2.value]);
+      console.log(label + ":Message posted to worker");
     };
   });
 
   myWorker.port.onmessage = (e) => {
     result1.textContent = e.data;
-    console.log("Message received from worker");
-    console.log(e.lastEventId);
+    console.log(label + ":Message received from worker");
+    console.log(label + ":" + e.lastEventId);
   };
+}
+
+if (!!window.SharedWorker) {
+    // Create with the extendedLifetime option.
+    const worker1 = new SharedWorker("worker.js", {name: "extendedLifetime", extendedLifetime: true});
+    setup("extendedLifetime", worker1, num1, num2, result1);
+
+    const worker2 = new SharedWorker("worker.js", {name: "legacy"});
+    setup("extendedLifetime", worker2, num3, num4, result2);
 }
