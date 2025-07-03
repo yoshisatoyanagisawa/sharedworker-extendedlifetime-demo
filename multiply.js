033a1328c30e48ec596a142743fb14a803440039
Yoshisato Yanagisawa <yoshisato.yanagisawa@gmail.com>
2025-07-03 19:24:19 +0900

    Create multiply.js

---
+++ multiply.js
@@ -0,0 +1,22 @@
+const first = document.querySelector("#number1");
+const second = document.querySelector("#number2");
+
+const result1 = document.querySelector(".result1");
+
+if (!!window.SharedWorker) {
+  // Create with the extendedLifetime option.
+  const myWorker = new SharedWorker("worker.js", {extendedLifetime: true});
+
+  [first, second].forEach((input) => {
+    input.onchange = () => {
+      myWorker.port.postMessage([first.value, second.value]);
+      console.log("Message posted to worker");
+    };
+  });
+
+  myWorker.port.onmessage = (e) => {
+    result1.textContent = e.data;
+    console.log("Message received from worker");
+    console.log(e.lastEventId);
+  };
+}
